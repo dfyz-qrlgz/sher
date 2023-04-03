5b14c70f4b51dccb7ca2c04f478042dc972e5459
DFYZ <ivan6179217@mail.ru>
2023-04-03 15:01:39 +0300

    shershen

---
+++ RACTICA_1_4/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using RACTICA_1_4.libraryDataSetTableAdapters;
+using RACTICA_1_4;
+using System.Data;
+using System.Net;
+using System.Xml.Linq;
+
+namespace RACTICA_1_4
+{
+    public partial class MainWindow : Window
+    {
+        authorsTableAdapter authors = new authorsTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            datagrid.ItemsSource = authors.GetData();
+        }
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            second window = new second();
+            window.ShowDialog();
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            string fString = Athid.Text;
+            string sString = AthName.Text;
+            string tString = AthMail.Text;
+
+            if (fString != "" && sString != "" && tString != "")
+            {
+                authors.InsertQuery(Convert.ToInt32(Athid.Text), (string)AthName.Text, (string)AthMail.Text);
+                datagrid.ItemsSource = authors.GetData();
+            }
+        }
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            if (datagrid.SelectedItem != null)
+            {
+            object Athid = (datagrid.SelectedItem as DataRowView).Row[0];
+            object AthName = (datagrid.SelectedItem as DataRowView).Row[1];
+            object AthMail = (datagrid.SelectedItem as DataRowView).Row[2];
+
+            authors.DeleteQuery(Convert.ToInt32(Athid), (string)AthName, (string)AthMail);
+            datagrid.ItemsSource = authors.GetData();
+            }
+            
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            if (datagrid.SelectedItem != null)
+            {
+                object AthId = (datagrid.SelectedItem as DataRowView).Row[0];
+
+                authors.UpdateQuery(Convert.ToInt32(Athid.Text), (string)AthName.Text, (string)AthMail.Text, Convert.ToInt32(Athid.Text));
+                datagrid.ItemsSource = authors.GetData();
+            }
+        }
+
+        private void datagrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (datagrid.SelectedItem != null)
+            {
+                Athid.Text = (datagrid.SelectedItem as DataRowView).Row[0].ToString();
+                AthName.Text = (datagrid.SelectedItem as DataRowView).Row[1].ToString();
+                AthMail.Text = (datagrid.SelectedItem as DataRowView).Row[2].ToString();
+            }
+        }
+    }
+}
---
+++ RACTICA_1_4/second.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using RACTICA_1_4.libraryDataSetTableAdapters;
+
+namespace RACTICA_1_4
+{
+
+    public partial class second : Window
+    {
+        booksTableAdapter books = new booksTableAdapter();
+        authorsTableAdapter authors = new authorsTableAdapter();
+        public second()
+        {
+            InitializeComponent();
+            datagrid2.ItemsSource = books.GetData();
+
+            Cmbox.ItemsSource = authors.GetData();
+            Cmbox.DisplayMemberPath = "author_id";
+        }
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            object BookId = (datagrid2.SelectedItem as DataRowView).Row[0];
+            books.DeleteQuery(Convert.ToInt32(BookId));
+            datagrid2.ItemsSource = books.GetData();
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            books.InsertQuery(Convert.ToInt32(Bookid.Text), BkName.Text, Convert.ToInt32(Cmbox.Text), PubDate.Text);
+            datagrid2.ItemsSource = books.GetData();
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            if (datagrid2.SelectedItem != null)
+            {
+                object BookId = (datagrid2.SelectedItem as DataRowView).Row[0];
+
+                books.UpdateQuery(Convert.ToInt32(Bookid.Text), (string)BkName.Text, Convert.ToInt32(Cmbox.Text), (string)PubDate.Text, Convert.ToInt32(BookId));
+                datagrid2.ItemsSource = books.GetData();
+            }
+
+        }
+
+        private void datagrid2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(datagrid2.SelectedItem != null)
+            {
+                Bookid.Text = (datagrid2.SelectedItem as DataRowView).Row[0].ToString();
+                BkName.Text = (datagrid2.SelectedItem as DataRowView).Row[1].ToString();
+                Cmbox.Text = (datagrid2.SelectedItem as DataRowView).Row[2].ToString();
+                PubDate.Text = (datagrid2.SelectedItem as DataRowView).Row[3].ToString();
+            }
+        }
+    }
+}
